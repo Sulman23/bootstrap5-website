d24286d6d74b0de2cb153c5f88e6b5fc52b9c5be
Sulman23 <sulmq23@gmail.com>
2021-11-06 13:11:07 +0500

    Almost Done!

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Navbar active 
+
+let nav = document.querySelector(".my-Nav");
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 20) {
+        nav.classList.add("scroll-on");
+    } else {
+        nav.classList.remove("scroll-on");
+    }
+}
