8a59842d9a3c04c613cb108cb70597e460b09521
Sulman23 <sulmq23@gmail.com>
2021-11-06 13:47:09 +0500

    deleted extra folder of images

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 // Navbar active 
 
-let nav = document.querySelector(".my-Nav");
-window.onscroll = function () {
-    if (document.documentElement.scrollTop > 20) {
-        nav.classList.add("scroll-on");
-    } else {
-        nav.classList.remove("scroll-on");
-    }
-}
+// let nav = document.querySelector(".my-Nav li");
+// window.onscroll = function () {
+//     if (document.documentElement.scrollTop > 20) {
+//         nav.classList.add("scroll-on");
+//     } else {
+//         nav.classList.remove("scroll-on");
+//     }
+// }
